72d05f81ad21c5704bde27aab8474b9a419d4ca1
Анна Смольянинова <anna.smln@mail.ru>
2022-09-14 00:09:37 +0300

    не работают картинки

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ import browser from 'browser-sync';
 import rename from 'gulp-rename';
 import htmlmin from 'gulp-htmlmin';
 import scoosh from 'gulp-libsquoosh';
+import svgo from 'svgo';
+import svgstore from 'svgstore';
+import del from 'del';
+
 
 // Styles
 
@@ -26,7 +30,7 @@ export const styles = () => {
 
 //html
 
-export const html = () => {
+const html = () => {
   return gulp.src('source/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('build'));
@@ -46,6 +50,77 @@ const server = (done) => {
   done();
 }
 
+//reload
+
+const reload = (done) => {
+  browser.reload();
+  done();
+}
+
+
+
+//script
+const script = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+}
+
+//images
+const images = () => {
+  return gulp.src('source/images/**/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/images'))
+}
+
+
+//webP
+
+const createWebP = () => {
+  return gulp.src('source/images/**/*.{jpg,png}')
+    .pipe(scoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('build/images'))
+}
+
+
+//svg
+
+const svg = () => {
+  return gulp.src(['source/images/logos/*.svg', 'source/images/icons/advantage-icons*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/images'))
+}
+
+const sprite = () => {
+  return (['source/images/slogan/*.svg', 'source/images/icons/**/*.svg', '!source/images/icons/advantage-icons*.svg'])
+    .pipe(svgo())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename(sprite.svg))
+    .pipe(gulp.dest('build/images'))
+}
+
+//copy
+
+const copy = () => {
+  gulp.src([
+    'source/fonts/*.{woff,woff2}',
+    'source/*.ico',
+  ], {
+    base: 'sourse'
+  })
+    .pipe(gulp.dest('build'))
+}
+
+//clean
+
+const clean = () => {
+  return del('build')
+}
+
 // Watcher
 
 const watcher = () => {
@@ -58,16 +133,18 @@ export default gulp.series(
   html, styles, server, watcher
 );
 
-//script
-export const script = () => {
-  return gulp.src('source/js/*.js')
-    .pipe(terser())
-    .pipe(gulp.dest('build/js'))
-}
+//build
 
-//images
-export const images = () => {
-  return gulp.src('source/images/**/*.{jpg,png}')
-    .pipe(squoosh())
-    .pipe(gulp.dest('build/images'))
-}
+export const build = gulp.series(
+  clean,
+  copy,
+  images,
+  gulp.parallel(
+    styles,
+    html,
+    svg,
+    sprite,
+    script,
+    createWebP
+  )
+)
